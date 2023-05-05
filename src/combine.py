df433dd1ffd02372de629ec504c5083745856488
knoriy <knoriy@ip-172-31-38-94.ec2.internal>
2023-05-05 11:27:25 +0000

    added signal handeling for preempt slurm nodes

--- src/combine.py
+++ src/combine.py
@@ -1,4 +1,6 @@
+import sys
 import PIL
+import json
 import torch
 import open_clip
 from whisper_jax import FlaxWhisperPipline
@@ -14,6 +16,15 @@ model, _, transform = open_clip.create_model_and_transforms(
 )
 model.to(device)
 
+
+#############
+# create global completed list
+complete = []
+#############
+
+
+
+
 def inference_caption(image, decoding_method="Beam search", rep_penalty=1.2, top_p=0.5, min_seq_len=5, seq_len=20):
     im = transform(image).unsqueeze(0).to(device)
     generation_type = "beam_search" if decoding_method == "Beam search" else "top_p"
@@ -28,8 +39,11 @@ def inference_caption(image, decoding_method="Beam search", rep_penalty=1.2, top
         )
     return open_clip.decode(generated[0].detach()).split("<end_of_text>")[0].replace("<start_of_text>", "")
 
-def main(urls:list[str]):
-    dataset = YoutubeTDM(train_urls=urls)
+def main(urls:list[str], exclude_list:list[str]=[]):
+    dataset = YoutubeTDM(
+        train_urls=urls,
+        exclude_list=exclude_list,
+        )
     dataset.setup()
 
     for video in dataset.train:
@@ -48,9 +62,29 @@ def main(urls:list[str]):
 
             caption = inference_caption(PIL.Image.fromarray(frame.numpy()))
             data.append({"caption": caption, "gender": None, "emotion": None, "text": chunk['text'], "timestamp": chunk["timestamp"]})
+
+        complete.append({"id": json_meta["id"], "data": data})
         break
 
     return data
+
+
+# Signal Handeling
+
+def terminateProcess(signalNumber, frame):
+    print ('(SIGTERM) terminating the process')
+
+    # Cache completed files
+    with open("completed.json", "w") as f:
+        json.dump(complete, f)
+
+    sys.exit()
+
 if __name__ == '__main__':
+    import signal
     import pprint
+
+    # catch signal
+    signal.signal(signal.SIGTERM, terminateProcess)
+
     pprint.pprint(main(["s3://s-laion/documentaries/00000/"]))
--- src/datamodule/datamodule.py
+++ src/datamodule/datamodule.py
@@ -9,23 +9,33 @@ from .base import BaseTDM
 from utils import read_video
 
 class YoutubeTDM(BaseTDM):
+	def __init__(self, exclude_list=[], *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.exclude_list = exclude_list
+
+	def filter_fn(self, data):
+		file_name = os.path.basename(data[0][0]).split(".")[0]
+		if file_name in self.exclude_list:
+			return False
+		return True
+
 	def to_sampels(self, data):
 		meta = data[0]
-		video = data[2]
-		audio = data[1]
+		video = data[1]
 		
-		video_frames, _, video_meta = read_video(video[1], pts_unit="sec", end_pts=5)
-		_, audio_frames, audio_meta = read_video(audio[1], pts_unit="sec", end_pts=5)
+		video_frames, audio_frames, av_meta = read_video(video[1], pts_unit="sec", end_pts=5)
 		json_meta = json.load(meta[1])
+		json_meta['filename'] = os.path.basename(video[0]).split(".")[0]
 
-		return (video_frames, audio_frames, video_meta|audio_meta), json_meta
+		return (video_frames, audio_frames, av_meta), json_meta
 
 	def create_pipeline(self, data_dir):
 		datapipe = torchdata.datapipes.iter.IterableWrapper(data_dir)\
 			.list_files_by_fsspec()\
 			.sharding_filter()\
 			.open_files_by_fsspec(mode='rb')\
-			.groupby(lambda x: os.path.basename(x[0]).split(".")[0], group_size=3, guaranteed_group_size=3)\
+			.groupby(lambda x: os.path.basename(x[0]).split(".")[0], group_size=2, guaranteed_group_size=2)\
+			.filter(self.filter_fn)\
 			.map(self.to_sampels) \
 
 		return datapipe
